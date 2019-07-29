806cb10111523caafbc472736f70f74f2fe4e446
Alb4Tr02 <834@holbertonschool.com>
2019-07-29 15:17:58 -0400

    _printf update fix betty

--- _printf.c
+++ _printf.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 
 /**
- * _printf - produces output according to a format.
- * @format: formats input.
+ * make_struct - produces output according to a format.
+ * @a: formats input.
  * Return: lenght of format.
  */
-
-int _printf(const char * const format, ...)
+void make_struct(prfor_t *a)
 {
 	prfor_t form_get[] = {
 		{"c", print_char},
@@ -17,48 +16,50 @@ int _printf(const char * const format, ...)
 		{"R", print_string_rot},
 		{NULL, NULL}
 	};
-	int i, j, lenght;
+	a = form_get;
+}
+/**
+ * _printf - produces output according to a format.
+ * @format: formats input.
+ * Return: lenght of format.
+ */
+
+int _printf(const char * const format, ...)
+{
+	prfor_t *form_get;
+	int i, j, lenght = 0, find = 0;
 	va_list argu;
 
+	make_struct(form_get);
 	va_start(argu, format);
-	lenght = 0;
-	int find = 0;
-
 	if (format == NULL)
 		return (-1);
 	for (i = 0; format != NULL && format[i]; i++)
 	{
 		if (format[i] != '%')
-		{
 			_putchar(format[i]);
-			lenght++;
-		}
 		else
 		{
-			i++;
-			j = 0;
+			i++, j = 0;
 			if (format[i] == '%')
-			{
 				_putchar('%');
-				lenght++;
-			}
 			find = 0;
 			for (; j < 6; j++)
 			{
 				if (form_get[j].form[0] == format[i])
 				{
-					lenght += (form_get[j].f(argu));
+					lenght += (form_get[j].f(argu)) - 1;
 					find = 1;
-					break;
 				}
 			}
 			if (!find && format[i] != '%')
 			{
-				lenght += 2;
+				lenght++;
 				_putchar('%');
 				_putchar(format[i]);
 			}
 		}
+		length++;
 	}
 	va_end(argu);
 	return (lenght);
