67b7fa013aa373ebdc670689fb0d9e8b787bfe34
David Peralta <924@holbertonschool.com>
2019-07-30 22:32:17 -0500

    update fix 7-print_pointer.c fix

--- 7-print_pointer.c
+++ 7-print_pointer.c
@@ -54,10 +54,10 @@ void imprimir(void)
 int print_pointer(va_list va)
 {
 	char bin[64];
-	unsigned long int punt;
-	unsigned long int t = 1, resp = 0;
+	unsigned long int punt, t = 1, resp = 0;
 	int i = 0, j = 0, p = 0, cont = 0, flag = 0;
- 	punt =va_arg(va, unsigned long int);
+
+	punt = va_arg(va, unsigned long int);
 	if (!punt)
 	{
 		imprimir();
